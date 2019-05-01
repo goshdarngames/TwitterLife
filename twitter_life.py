7a9686aa43cc264eb426f0e917ca70be62be375b
Your Name <you@example.com>
2019-05-01 17:55:28 +0100

    Image is built and run but output is not yet printed to the console.

---
+++ twitter_life.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+#############################################################################
+# twitter_life.py
+#----------------------------------------------------------------------------
+# Running genetic algorithms and posting the results on twitter.
+#############################################################################
+
+import docker
+
+import os, logging, sys
+
+#----------------------------------------------------------------------------
+
+SMART_ROCKETS_PATH = os.path.join ( ".", "smart_rockets" )
+SMART_ROCKETS_TAG = "smart_rockets" 
+
+#----------------------------------------------------------------------------
+
+def main ():
+
+    logging.info ( "Building Smart Rockets." )
+
+    docker_client = docker.from_env ()
+
+    smart_rocket_build_tuple = \
+            docker_client.images.build ( path = SMART_ROCKETS_PATH,
+                                         tag  = SMART_ROCKETS_TAG ) 
+
+    smart_rocket_img = smart_rocket_build_tuple [ 0 ]
+
+    logging.info ( "Running Smart Rockets." )
+
+    output = docker_client.containers.run ( smart_rocket_img.id )
+
+    logging.info ( "Output: "+str(output) )
+
+
+if __name__ == '__main__':
+
+    sys.exit ( main () )
